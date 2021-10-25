7b2dd90969266b88f5910075b47cbd729739f96a
palani3 <46318979+palani3@users.noreply.github.com>
2021-10-25 20:49:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './components/Demo';
+import Table from './components/Table';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
